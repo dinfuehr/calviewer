3bad672414638bf8478386a19974af97583605f2
Dominik Inführ <dominik.infuehr@gmail.com>
2015-08-22 13:46:34 -0700

    grouped year into months

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 extern crate chrono;
+extern crate itertools;
 
 use chrono::*;
-use iter::date_iter;
+use iter::*;
+use itertools::Itertools;
 
 mod iter;
 
 fn main() {
-    let start = Local.ymd(2015, 1, 1);
-    let end = Local.ymd(2016, 1, 1);
+    let year = dates_in_year(2015);
+    let months = year.group_by_lazy(|d| d.month());
 
-    for date in date_iter(start, end) {
-        println!("date = {}", date);
+    for (month, dates) in months.into_iter() {
+        println!("group {} with {} dates", month, dates.into_iter().count());
     }
+}
+
+fn dates_in_year(year: i32) -> DateIterator<Local> {
+    let start = Local.ymd(year, 1, 1);
+    let end = Local.ymd(year+1, 1, 1);
+
+    date_iter(start, end)
 }
