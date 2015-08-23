8b11cb52c4f27dc6819b547024406ca56955fd55
Dominik Inführ <dominik.infuehr@gmail.com>
2015-08-22 17:53:03 -0700

    added ruby solution
    unfortunately not lazy

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,16 +3,21 @@ extern crate itertools;
 
 use chrono::*;
 use iter::*;
-use itertools::Itertools;
+use itertools::*;
 
 mod iter;
 
 fn main() {
     let year = dates_in_year(2015);
     let months = year.group_by_lazy(|d| d.month());
+    let months_by_weeks = months.into_iter().map(|(_, days)| {
+        days.group_by_lazy(|d| d.isoweekdate().1)
+    });
 
-    for (month, dates) in months.into_iter() {
-        println!("group {} with {} dates", month, dates.into_iter().count());
+    for month in months_by_weeks {
+        for (week, days) in month.into_iter() {
+            println!("wday = {}, # days = {}", week, days.into_iter().count());
+        }
     }
 }
 
